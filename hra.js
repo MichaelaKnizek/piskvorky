12a866434ae096a51ca5d9fac22206b337456ddb
Michaela Knížková <michaela.knizek@gmail.com>
2025-04-17 18:08:57 +0200

    Stridani cricle a cross s kazdym kliknutim

--- hra.js
+++ hra.js
@@ -1,1 +1,16 @@
 let currentPlayer = 'circle';
+
+const handleClick = (event) => {
+  if (currentPlayer === 'circle') {
+    event.target.classList.add('game__square--circle');
+    currentPlayer = 'cross';
+  } else {
+    event.target.classList.add('game__square--cross');
+    currentPlayer = 'circle';
+  }
+};
+
+const buttons = document.querySelectorAll('.game-square');
+buttons.forEach((button) => {
+  button.addEventListener('click', handleClick);
+});
