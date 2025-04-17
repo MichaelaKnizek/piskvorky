46b383d2f8a3d138442703e22416f2989534344e
Michaela Knížková <michaela.knizek@gmail.com>
2025-04-17 23:35:12 +0200

    Uprava znazorneni kdo prave hraje

--- hra.js
+++ hra.js
@@ -1,14 +1,18 @@
 let currentPlayer = 'circle';
 
-let kdoHraje = document.querySelector('.square');
+const kdoHraje = document.querySelector('.symbol');
 
 const handleClick = (event) => {
+  kdoHraje.classList.remove('symbol', 'symbol__circle', 'symbol__cross');
+
   if (currentPlayer === 'circle') {
     event.target.classList.add('game__square--circle');
     currentPlayer = 'cross';
+    kdoHraje.classList.add('symbol__cross');
   } else {
     event.target.classList.add('game__square--cross');
     currentPlayer = 'circle';
+    kdoHraje.classList.add('symbol__circle');
   }
   event.target.disabled = true;
 };
