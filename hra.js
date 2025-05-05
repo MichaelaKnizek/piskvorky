2bff392d83264183936886f612cfe4914c8f7a61
Michaela Knížková <michaela.knizek@gmail.com>
2025-05-05 12:33:56 +0200

    Vytvoreni pole pro fci findWinner

--- hra.js
+++ hra.js
@@ -3,6 +3,7 @@ import { findWinner } from 'https://unpkg.com/piskvorky@0.1.4';
 let currentPlayer = 'circle';
 
 const kdoHraje = document.querySelector('.symbol');
+const buttons = document.querySelectorAll('.game-square');
 
 const handleClick = (event) => {
   kdoHraje.classList.remove('symbol', 'symbol__circle', 'symbol__cross');
@@ -17,9 +18,20 @@ const handleClick = (event) => {
     kdoHraje.classList.add('symbol__circle');
   }
   event.target.disabled = true;
+
+  const herniPole = Array.from(buttons).map((button) => {
+    if (button.classList.contains('game__square--circle')) {
+      return 'o';
+    } else if (button.classList.contains('game__square--cross')) {
+      return 'x';
+    } else {
+      return '_';
+    }
+  });
+
+  console.log(herniPole);
 };
 
-const buttons = document.querySelectorAll('.game-square');
 buttons.forEach((button) => {
   button.addEventListener('click', handleClick);
 });
